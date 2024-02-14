c338b64402a9c358c12332d4f6faad697dcfdf7d
louis mahe <lmahe@made-f0Br5s3.clusters.42paris.fr>
2024-02-14 18:12:01 +0100

    plane and sphere

---
+++ srcs/math/plane_inter.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plane_inter.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmahe <lmahe@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 14:56:02 by lmahe             #+#    #+#             */
+/*   Updated: 2024/02/14 18:10:47 by lmahe            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minirt/types.h"
+#include "../../includes/minirt/calculus.h"
+
+
+/// @brief check if a given ray intersect a given plane. If so update the ray data.
+/// @param plane
+/// @param ray
+void	plane_intersec(t_object *plane, t_ray *ray)
+{
+	double	a;
+	double	b;
+	double	c;
+
+	if (plane->type != PLAN)
+		return ;
+	a = scalar_product(&ray->dir, &plane->orientation);
+	c = scalar_product(&plane->orientation, &plane->origin);
+	b = scalar_product(&plane->orientation, &ray->origin) - c;
+	if (linear_solver(a, b, &c) && c < ray->t)
+	{
+		ray->t = c;
+		ray->normal = plane->orientation;
+		normalize_vec(&ray->normal);
+	}
+}
+
