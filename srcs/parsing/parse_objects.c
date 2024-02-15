7bd1d2edff6c84f693b3b9eba5312781dbb1af2f
louis mahe <lmahe@made-f0Br5s3.clusters.42paris.fr>
2024-02-15 13:15:53 +0100

    parsing: stock rayons et haut et bas cylindre

--- srcs/parsing/parse_objects.c
+++ srcs/parsing/parse_objects.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_objects.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: npirard <npirard@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lmahe <lmahe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 18:08:26 by npirard           #+#    #+#             */
-/*   Updated: 2024/02/14 11:35:26 by npirard          ###   ########.fr       */
+/*   Updated: 2024/02/15 13:10:30 by lmahe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 #include <minirt/minirt.h>
 #include <minirt/parsing.h>
 #include <minirt/objects.h>
+#include <minirt/calculus.h>
 
 int	scene_parse_sphere(t_object *object, int nbr, char **ptr, char *line)
 {
 	if (scene_parse_origin(ptr, &object->origin))
 		return (error_parsing("sphere origin", nbr, line), 1);
-	if (scene_parse_double(ptr, &object->diameter))
+	if (scene_parse_double(ptr, &object->radius))
 		return (error_parsing("sphere diameter", nbr, line), 1);
+	object->radius /= 2;
 	if (scene_parse_color(ptr, &object->color))
 		return (error_parsing("sphere color", nbr, line), 1);
 	return (0);
@@ -39,14 +41,20 @@ int	scene_parse_plan(t_object *object, int nbr, char **ptr, char *line)
 
 int	scene_parse_cylinder(t_object *object, int nbr, char **ptr, char *line)
 {
+	t_vec3f	temp;
+
 	if (scene_parse_origin(ptr, &object->origin))
 		return (error_parsing("cylinder origin", nbr, line), 1);
 	if (scene_parse_orientation(ptr, &object->orientation))
 		return (error_parsing("cylinder orientation", nbr, line), 1);
-	if (scene_parse_double(ptr, &object->diameter))
+	if (scene_parse_double(ptr, &object->radius))
 		return (error_parsing("cylinder diameter", nbr, line), 1);
+	object->radius /= 2;
 	if (scene_parse_double(ptr, &object->height))
 		return (error_parsing("cylinder height", nbr, line), 1);
+	temp = vec3_scale(&object->orientation, object->height / 2);
+	object->top = vec3_sum(&object->origin, &temp);
+	object->origin = vec3_diff(&object->origin, &temp);
 	if (scene_parse_color(ptr, &object->color))
 		return (error_parsing("cylinder color", nbr, line), 1);
 	return (0);
